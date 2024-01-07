b2befef0c7c5193171cb259b295af0bd12529605
lyokocode <aelita.dev@gmail.com>
2024-01-07 05:45:06 +0300

    react native <Image/>

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
@@ -13,7 +13,16 @@ export const HomeScreen = () => {
 
     return (
         <SafeAreaView>
-            <Text className="text-red-500">HomeScreen</Text>
+            {/* header */}
+            <View className="mt-8" >
+                <Image
+                    className="w-10 h-10 rounded-full bg-gray-300"
+                    source={{
+                        uri: "https://pbs.twimg.com/profile_images/1740476733659254785/S3chCct-_400x400.jpg"
+                    }}
+                />
+
+            </View>
         </SafeAreaView>
     )
 }
