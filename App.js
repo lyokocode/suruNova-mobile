660a6244e07701b9ba18658d69cc86bb29e1ceba
lyokocode <aelita.dev@gmail.com>
2024-01-07 03:01:44 +0300

    tailwind css with react-native

--- App.js
+++ App.js
@@ -3,18 +3,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hello Aelita</Text>
+    <View className="flex-1 items-center justify-center bg-red-300">
+      <Text className="text-xl text-purple-700">Hello Aelita!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
